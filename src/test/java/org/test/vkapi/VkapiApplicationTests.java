73377e9d3b1126e2ba47bd57b25ed0fb391915b6
Eset Shabakhov <eset-2000@mail.ru>
2025-02-01 18:55:41 +0500

    Добавлен запуск приложения и БД с помощью docker-compose.yml

--- src/test/java/org/test/vkapi/VkapiApplicationTests.java
+++ src/test/java/org/test/vkapi/VkapiApplicationTests.java
@@ -1,13 +1,13 @@
-package org.test.vkapi;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class VkapiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package org.test.vkapi;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class VkapiApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
